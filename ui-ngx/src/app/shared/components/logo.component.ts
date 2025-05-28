6ce90a0c4603bdd19637acfd2455f589bae83d56
JECanelos <juan.canelos@mosaicfactor.com>
2025-05-28 15:45:21 -0500

    NanoChronia title, logo and colors update

--- ui-ngx/src/app/shared/components/logo.component.ts
+++ ui-ngx/src/app/shared/components/logo.component.ts
@@ -23,10 +23,10 @@ import { Component } from '@angular/core';
 })
 export class LogoComponent {
 
-  logo = 'assets/logo_title_white.svg';
+  logo = 'assets/nanochronia-logo-login.png';
 
-  gotoThingsboard(): void {
-    window.open('https://thingsboard.io', '_blank');
+  gotoWebpage(): void {
+    window.open('https://nanochronia.com', '_blank');
   }
 
 }
